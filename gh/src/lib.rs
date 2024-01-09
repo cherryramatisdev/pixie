59069c3dabf3faed9b60f7c7a61043e250d4dd02
Cherry Ramatis <cherry.ramatis@icloud.com>
2024-01-09 20:17:12 -0300

    feat: add support to switch accounts using `gh` cli

--- gh/src/lib.rs
+++ gh/src/lib.rs
@@ -39,3 +39,39 @@ pub mod pr {
         }
     }
 }
+
+pub mod profile {
+    use std::{
+        env,
+        fs::OpenOptions,
+        io::Write,
+        path::PathBuf,
+        process::Command,
+    };
+
+    pub fn switch() -> anyhow::Result<()> {
+        Command::new("gh").args(["auth", "switch"]).output()?;
+
+        let home_dir = env::var("HOME").expect("HOME directory doesn't exist");
+        let cache_target_path = PathBuf::from(format!("{}/.local/share/gh_profile.txt", home_dir));
+
+        let mut file = OpenOptions::new()
+            .write(true)
+            .create(true)
+            .open(cache_target_path)?;
+
+        let output = Command::new("gh").args(["auth", "status"]).output()?;
+
+        if !output.status.success() {
+            return Err(anyhow::anyhow!(
+                "Couldn't run the github auth status command"
+            ));
+        }
+
+        let output = String::from_utf8_lossy(&output.stdout).to_string();
+
+        write!(file, "{}", output)?;
+
+        Ok(())
+    }
+}
